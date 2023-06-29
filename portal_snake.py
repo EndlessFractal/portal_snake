f76a57bf6d3429adb96c8ef99e6f8176c5e98e5f
Mr. T <89462237+NotoriusNeo@users.noreply.github.com>
2023-06-29 13:27:51 +0100

    Update portal_snake.py

--- portal_snake.py
+++ portal_snake.py
@@ -10,6 +10,7 @@ def resource_path(*paths):
     base_path = getattr(sys, '_MEIPASS', os.getcwd())
     return os.path.join(base_path, *paths)
 
+
 def game():
     # Initialize pygame and set screen size
     pygame.init()
@@ -80,7 +81,7 @@ def game():
     score = 0
 
     # Set font for displaying score
-    font_style = pygame.font.SysFont("Arial", 20)
+    font_style = pygame.font.SysFont("Arial", 30)
 
     # Create a list to store food rects
     food_rects = []
@@ -88,22 +89,26 @@ def game():
     # Function to generate food at random locations
     def generate_food():
         if len(food_rects) == 0:
-            for _ in range(5):
+            snake_positions = set((rect.x, rect.y) for rect in snake_body)
+            existing_food_positions = set((food.x, food.y) for food in food_rects)
+            new_food_rects = []
+            while len(new_food_rects) < 5:
                 food_rect = pygame.Rect(
                     round(random.randrange(0, width - block_size) / block_size) * block_size,
                     round(random.randrange(0, height - block_size) / block_size) * block_size,
                     block_size,
                     block_size,
                 )
-                # Check if food is going to be generated inside snake
-                food_collision = any(snake_block.colliderect(food_rect) for snake_block in snake_body)
-                if not food_collision:
-                    food_rects.append(food_rect)
+                if (food_rect.x, food_rect.y) not in snake_positions and (food_rect.x, food_rect.y) not in existing_food_positions:
+                    new_food_rects.append(food_rect)
+                    existing_food_positions.add((food_rect.x, food_rect.y))
+            food_rects.extend(new_food_rects)
 
     # Function to display score on screen
     def show_score(score):
-        score_text = font_style.render("Score: " + str(score), True, white)
-        screen.blit(score_text, [0, 0])
+        score_text = font_style.render(str(score), True, white)
+        score_text_rect = score_text.get_rect(center=(width // 2, 20))
+        screen.blit(score_text, score_text_rect)
 
     # Game over function
     def game_over():
@@ -113,8 +118,53 @@ def game():
         pygame.display.update()
         pygame.time.wait(2000)
         pygame.quit()
+        sys.exit()
 
     key_queue = []
+
+    # Flag to indicate if AI is in control
+    ai_control = False
+
+    # BFS AI function to find the shortest path to food
+    def bfs_ai(snake_rect, food_rects, snake_body):
+        distance_map = {}  # Dictionary to store shortest distances to positions
+        queue = deque([(snake_rect.x, snake_rect.y, 0, [])])  # Initialize the queue with snake's current position and distance
+
+        snake_body_positions = set((rect.x, rect.y) for rect in snake_body)  # Set to store snake's body positions
+
+        while queue:
+            x, y, distance, path = queue.popleft()  # Dequeue the next position from the queue
+
+            if (x, y) in distance_map and distance >= distance_map[(x, y)]:  # Skip if a shorter path has already been found
+                continue
+
+            distance_map[(x, y)] = distance  # Update the shortest distance for the position
+
+            if any(x == food.x and y == food.y for food in food_rects):  # Check if position contains food
+                return path  # Return the path to food
+
+            # Calculate the Manhattan distance between the current position and all adjacent positions
+            adjacent_positions = [
+                ((x + block_size) % width, y, distance + 1, path + ['right']),
+                ((x - block_size) % width, y, distance + 1, path + ['left']),
+                (x, (y + block_size) % height, distance + 1, path + ['down']),
+                (x, (y - block_size) % height, distance + 1, path + ['up'])
+            ]
+
+            # Sort adjacent positions based on their Manhattan distance to the food
+            adjacent_positions.sort(key=lambda pos: manhattan_distance(pos[:2], get_closest_food(food_rects, pos[:2])))
+
+            for pos in adjacent_positions:
+                if pos[:2] not in snake_body_positions:  # Skip positions that are part of snake's body
+                    queue.append(pos)  # Enqueue the valid position
+
+        return []  # Return empty path if no valid path is found
+
+    def manhattan_distance(pos1, pos2):
+        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+    def get_closest_food(food_rects, position):
+        return min(food_rects, key=lambda food: manhattan_distance(position, (food.x, food.y)))
 
     # Run the game loop
     generate_food()
@@ -122,9 +172,29 @@ def game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                quit()
+                sys.exit()
             elif event.type == pygame.KEYDOWN:
-                key_queue.append(event.key)
+                if event.key == pygame.K_v:
+                    ai_control = not ai_control  # Toggle AI control
+                elif not ai_control:
+                    key_queue.append(event.key)
+
+        if ai_control:
+            # AI Control
+            if not key_queue:
+                path = bfs_ai(snake_rect, food_rects, snake_body)
+
+                if path:
+                    next_move = path[0]
+
+                    if next_move == 'right' and last_direction != "left":
+                        direction = "right"
+                    elif next_move == 'left' and last_direction != "right":
+                        direction = "left"
+                    elif next_move == 'up' and last_direction != "down":
+                        direction = "up"
+                    elif next_move == 'down' and last_direction != "up":
+                        direction = "down"
 
         while key_queue:
             key = key_queue.pop(0)
@@ -160,7 +230,7 @@ def game():
         draw_tiled_background(screen, background_surface)
 
         # Check for collision with food
-        for food in food_rects:
+        for food in food_rects.copy():
             if snake_rect.colliderect(food):
                 sfx.play()
                 score += 1
@@ -190,6 +260,7 @@ def game():
 
         pygame.display.update()
         clock.tick(snake_speed)
+
 
 if __name__ == "__main__":
     game()
